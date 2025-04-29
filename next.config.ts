a3ffb48a6fe683cdb7f07268afd09bb1dd1bd4b4
HeshamMoawad <heshammoawad120120@yahoo.com>
2025-04-29 17:41:23 +0300

    🔧 update image pathname

--- next.config.ts
+++ next.config.ts
@@ -14,8 +14,8 @@ const nextConfig: NextConfig = {
         protocol: "http",
         hostname: "127.0.0.1",
         port: "8000",
-        pathname: "/media/**",
-      },
+        pathname: "/**",
+      }
     ],
   },
  
