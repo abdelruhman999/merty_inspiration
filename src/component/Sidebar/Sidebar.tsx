99dbef530fb170fe5eb31e802b410370b665db5b
HeshamMoawad <heshammoawad120120@yahoo.com>
2025-05-23 12:45:29 +0300

    🚀

--- src/component/Sidebar/Sidebar.tsx
+++ src/component/Sidebar/Sidebar.tsx
@@ -1,7 +1,10 @@
 'use client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { FaHome, FaBox, FaUsers, FaShoppingCart, FaCog, FaChartLine } from 'react-icons/fa';
+import { FaHome, FaBox , FaShoppingCart, FaCog } from 'react-icons/fa';
+import { MdAnalytics } from "react-icons/md";
+import { FaSignOutAlt } from "react-icons/fa";
+import Swal from 'sweetalert2';
 
 export function ClientSidebar() {
     const pathname = usePathname();
@@ -10,11 +13,12 @@ export function ClientSidebar() {
         { href: '/kashir', label: 'الرئيسية', icon: FaHome },
         { href: '/kashir/product-list', label: 'المنتجات', icon: FaBox },
         { href: '/kashir/orders', label: 'الطلبات', icon: FaShoppingCart },
-        { href: '/kashir/analytics', label: 'الإحصائيات', icon: FaChartLine },
-    ];
+        { href: '/kashir/analytics', label: 'الاحصائيات', icon: MdAnalytics },
+    ];0
 
     return (
-        <div className="h-screen w-64 bg-gray-800 text-white fixed right-0 top-0 p-4 overflow-y-auto" dir="rtl">
+        <div className="h-screen w-64 pb-[70px] flex flex-col justify-between bg-gray-800 text-white fixed right-0 top-0 p-4 overflow-y-auto" dir="rtl">
+          <div>
             <div className="mb-8">
                 <h1 className="text-2xl font-bold text-center">لوحة التحكم</h1>
             </div>
@@ -41,6 +45,33 @@ export function ClientSidebar() {
                     })}
                 </ul>
             </nav>
+          </div>
+
+
+            <div
+            onClick={async () => {
+                const result = await Swal.fire({
+                title: 'تأكيد الخروج',
+                text: 'هل أنت متأكد أنك تريد تسجيل الخروج؟',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'نعم، سجل الخروج',
+                cancelButtonText: 'إلغاء',
+                confirmButtonColor: '#d33',
+                cancelButtonColor: '#3085d6',
+                });
+
+                if (result.isConfirmed) {
+                document.cookie = 'sessionid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+                
+                window.location.href = '/kashir-login';
+                }
+            }}
+            className={`flex items-center gap-2 p-3 rounded-lg transition-colors bg-red-500/90 cursor-pointer text-white`}
+            >
+            <FaSignOutAlt />
+            تسجيل الخروج
+            </div>
         </div>
     );
 }
