e40794b9260cf4c1b09988584c59514aeef895dc
HeshamMoawad <heshammoawad120120@yahoo.com>
2025-05-31 02:49:39 +0300

    🚀

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
-module.exports = {
-  siteUrl: 'https://merty-inspiration.com',
-  generateRobotsTxt: true,
-};
+// module.exports = {
+//   siteUrl: 'https://merty-inspiration.com',
+//   generateRobotsTxt: true,
+// };
