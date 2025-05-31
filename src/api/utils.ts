89a0e77e418becde9cf26640e966f07a4113b143
HeshamMoawad <heshammoawad120120@yahoo.com>
2025-05-31 03:15:56 +0300

    🐛

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,5 +1,8 @@
 import {Base_Url} from "../calls/constant";
 
 export const serve = (url:string)=>{
+    if(url.startsWith("/") && Base_Url === ""){
+        return `https://merty-inspiration.com${url}`
+    }
     return `${url}`
 }
